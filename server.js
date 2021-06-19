82d7ceee8aa0425e7cf827658226b0f2a46ac5c2
payalpatra <payalpatra105@gmail.com>
2021-06-20 02:49:07 +0530

    Heroku deployment

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 require("dotenv").config();
-// const path = require("path")
+const path = require("path")
 const express = require("express");
 const cors = require("cors");
 
 // Controllers
-const createWallet = require("./controllers/createWallet");
-const transfer = require("./controllers/transfer");
-const confirmation = require("./controllers/confirmation");
-const listTransactions = require("./controllers/listTransactions");
-const listPayments = require("./controllers/listPayments");
-const allWallets = require("./controllers/allWallets");
-const allCustomers = require("./controllers/allCustomers");
-const createCustomer = require("./controllers/createCustomer");
-const createPayment = require("./controllers/createPayment");
-const feedback = require("./controllers/feedback");
+const createWallet = require("./backend/controllers/createWallet");
+const transfer = require("./backend/controllers/transfer");
+const confirmation = require("./backend/controllers/confirmation");
+const listTransactions = require("./backend/controllers/listTransactions");
+const listPayments = require("./backend/controllers/listPayments");
+const allWallets = require("./backend/controllers/allWallets");
+const allCustomers = require("./backend/controllers/allCustomers");
+const createCustomer = require("./backend/controllers/createCustomer");
+const createPayment = require("./backend/controllers/createPayment");
+const feedback = require("./backend/controllers/feedback");
 
 // MongoDB Cloud Connection
-const conectDB = require("./config/db");
+const conectDB = require("./backend/config/db");
 
 // Initializing APP
 const app = express();
@@ -62,22 +62,20 @@ app.get("/api/allCustomers", allCustomers);
 // --------------------------- Save Feedback ---------------------------- \\
 app.post("/api/feedback", feedback);
 
-app.get("/", (req, res) => {
-  res.send("Hey There !! Welcome to the server");
-});
+
 
 // Production
 
-// if(process.env.NODE_ENV === 'production'){
-//     app.use(express.static(path.join(__dirname, '/frontend/build')));
-//     app.get("*", (req, res)=>{
-//      res.sendFile(path.join(__dirname, 'frontend',"build","index.html"));
-//     })
-//   }else{
-//     app.get("/", (req,res)=>{
-//       res.send("Hey There , Greetings From The Server. Have a Good Day :)")
-//     })
-//   }
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+    app.get("*", (req, res)=>{
+     res.sendFile(path.join(__dirname, 'frontend',"build","index.html"));
+    })
+  }else{
+    app.get("/", (req,res)=>{
+      res.send("Hey There , Greetings From The Server. Have a Good Day :)")
+    })
+  }
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("serve at http://localhost:5000"));
